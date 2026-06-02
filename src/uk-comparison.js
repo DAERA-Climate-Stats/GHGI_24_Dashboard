30679fdbeee743675adc27ba0a0380836e13291a
DARCY-NISRA <paul.darcy@nisra.gov.uk>
2026-06-02 12:42:00 +0100

    update colour and remove about drop down box

--- src/uk-comparison.js
+++ src/uk-comparison.js
@@ -266,11 +266,11 @@ window.addEventListener("DOMContentLoaded", async () => {
     
     // Populate info boxes
     populateInfoBoxes(
-        ["Definitions", "Source", "What does the data mean?"],
+        ["Definitions", /*"Source",*/ "What does the data mean?"],
         [
         `<p><strong>CO&#8322; Equivalent</strong>: The values in this dashboard are presented in CO&#8322; equivalent units (CO&#8322;e). This is the emissions value, weighted using the appropriate GWP for the gas type. This standardises emissions from different gases, allowing comparison. More information on GWPs is available at: UK greenhouse gas emissions: other technical reports.</p>`,
 
-        `<strong>MetaData</strong>
+        /*`<strong>MetaData</strong>
         <p>The data in this dashboard come from <a href="https://naei.energysecurity.gov.uk/reports/greenhouse-gas-inventories-england-scotland-wales-northern-ireland-1990-2023" target="_blank" rel="noopener">Greenhouse Gas Inventories for England Scotland, Wales and Northern Ireland</a></p>
         <p>The GHG inventory data are produced annually by Ricardo Energy and Environment, on behalf of the Department for Energy Security & Net Zero, the Scottish Government, the Welsh Assembly Government and the Northern Ireland Department of Agriculture, Environment and Rural Affairs. Each year the GHG inventory is extended and updated. The entire historical data series, from 1990 to the latest year, is revised to incorporate methodological improvements and new data. This takes into account revisions to the datasets used in its compilation.</p>
         <p>The GHG emission estimates are based on a wide range of data sources and sources of uncertainty include statistical differences, assumptions, proxy datasets and expert judgement. In addition, the natural variability in the processes that are being modelled introduce uncertainty. For example, carbon content of fuels and farming practices under different climatic conditions and soil types. Uncertainty estimates for Northern Ireland emissions are available for the base year, the latest year (2023) and for the percentage change between the two years. For the base year, a close approximation of the 95% confidence interval is ±8%, and for 2023 it is ±7%. For the percentage reduction between the base year and 2023, the 95% confidence interval ranges from 20% to 37%.</p>
@@ -281,7 +281,7 @@ window.addEventListener("DOMContentLoaded", async () => {
         <p>Further information can be found on the <a href="https://www.daera-ni.gov.uk/landing-pages/statistics" target="_blank" rel="noopener">DAERA Statistics website</a>.</p>
         <p>Date of Publication:</p>
         <p>Published: Annually</p>
-        <p>Time period covered - Greenhouse Gas Inventory: 1990-${latest_year}</p>`,
+        <p>Time period covered - Greenhouse Gas Inventory: 1990-${latest_year}</p>`,*/
 
         `<p>This page compares greenhouse gas emissions across the UK devolved administrations. A logarithmic scale may be used to allow emissions of very different magnitudes to be shown on the same chart.</p>`
         
